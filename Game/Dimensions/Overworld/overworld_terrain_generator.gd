002b36c8278ddcd00f438b63e6b219da91f63254
Darco <darcowb388@gmail.com>
2025-07-20 16:11:18 +0530

    fixied falling to void

--- Game/Dimensions/Overworld/overworld_terrain_generator.gd
+++ Game/Dimensions/Overworld/overworld_terrain_generator.gd
@@ -2,12 +2,13 @@ extends VoxelGeneratorScript
 
 var noise: Noise = FastNoiseLite.new()
 
-const WORLD_HEIGHT: int = 20
+const WORLD_HEIGHT: int = 300
 
 func _init():
 	noise.noise_type = FastNoiseLite.TYPE_PERLIN
+	noise.frequency = 0.006 
 	noise.seed = 123456
-	noise.frequency = 0.01
+
 
 func _generate_block(buffer: VoxelBuffer, origin: Vector3i, lod: int) -> void:
 	var size = buffer.get_size()
@@ -17,14 +18,46 @@ func _generate_block(buffer: VoxelBuffer, origin: Vector3i, lod: int) -> void:
 		for x in size.x:
 			var global_voxel_position_x = origin.x + x
 			var global_voxel_position_y = origin.z + z
-			var height = int(noise.get_noise_2d(global_voxel_position_x, global_voxel_position_y) * WORLD_HEIGHT + WORLD_HEIGHT)
+			var height = int((noise.get_noise_2d(global_voxel_position_x, global_voxel_position_y) + 1.0) * 32.0 + 32.0)
+
+			
 			for y in size.y:
 				var world_y = origin.y + y
+				var block_type = TerrainData.AIR
 				if world_y > height:
 					continue
 				elif world_y > height - 1:
-					buffer.set_voxel(TerrainData.GRASS, x, y, z, channel)
+					block_type = TerrainData.GRASS
 				elif world_y > height - 5: 
-					buffer.set_voxel(TerrainData.DIRT, x, y, z, channel)
+					block_type = TerrainData.DIRT
 				else:
-					buffer.set_voxel(TerrainData.STONE, x, y, z, channel)
+					block_type = TerrainData.STONE
+				
+				buffer.set_voxel(block_type, x, y, z, channel)
+				
+				if world_y == height and world_y < 70 and world_y > 60 and randi() % 50 == 0:
+					spawn_tree(buffer, Vector3i(x, y + 1, z))
+
+func spawn_tree(buffer: VoxelBuffer, pos: Vector3i):
+	var wood_type = 5
+	var leaf_type = 4
+	
+	for i in 3:
+		var trunk_pos = pos + Vector3i(0, i, 0)
+		if is_in_bounds(buffer, trunk_pos):
+			buffer.set_voxel(wood_type, trunk_pos.x, trunk_pos.y, trunk_pos.z, VoxelBuffer.CHANNEL_TYPE)
+
+	for dx in [-1, 0 , 1]:
+		for dz in [-1, 0, 1]:
+				if dx != 0 or dz != 0:
+					var leaf_pos = pos + Vector3i(dx, 3, dz)
+					if is_in_bounds(buffer, leaf_pos):
+						buffer.set_voxel(leaf_type, leaf_pos.x, leaf_pos.y, leaf_pos.z, VoxelBuffer.CHANNEL_TYPE)
+
+	
+	
+func is_in_bounds(buffer: VoxelBuffer, pos: Vector3i) -> bool:
+	var size = buffer.get_size()
+	return pos.x >= 0 and pos.x < size.x and \
+		   pos.y >= 0 and pos.y < size.y and \
+		   pos.z >= 0 and pos.z < size.z
--- Game/Entities/Player/block_break_n_placement.gd
+++ Game/Entities/Player/block_break_n_placement.gd
@@ -8,6 +8,8 @@ extends Node3D
 @export var coord_label: Label
 
 var outline_cube: MeshInstance3D
+var collision_check_body: Area3D
+
 var voxel_terrain: VoxelTerrain
 var voxel_tool: VoxelTool
 
@@ -16,15 +18,50 @@ var current_target_block_normal: Vector3
 
 func _ready() -> void:
 	outline_cube = create_outline_cube()
+	collision_check_body = create_outline_block_collision()
+	
 	if Data.world.voxel_terrain: voxel_terrain = Data.world.voxel_terrain
 	voxel_tool = voxel_terrain.get_voxel_tool()
 
 func _process(delta: float) -> void:
+	update_collision_check_body()
+	update_outline()
 	block_deystroy()
 	block_placement()
-	update_outline()
 	
 	coord_label.text = str(player.position)
+
+
+## <--- BLOCK PLACEMENT AND DEYSTORY ---> ##
+
+func block_deystroy():
+	if Input.is_action_just_pressed("attack"):
+		var current_voxel_type = voxel_tool.get_voxel(current_targeted_block)
+		if current_voxel_type != TerrainData.AIR:
+			voxel_tool.set_voxel(current_targeted_block, TerrainData.AIR)
+
+func block_placement():
+	if Input.is_action_just_pressed("interact") and player_inventory.selected_item != null and can_place_block():
+		var item: Item = player_inventory.selected_item
+		
+		if !item.can_be_placed: return
+		
+		var place_block_type = item.block_model
+		var tool = voxel_terrain.get_voxel_tool()
+
+		tool.set_channel(VoxelBuffer.CHANNEL_TYPE)
+
+		var place_pos = current_targeted_block + Vector3i(current_target_block_normal)
+
+		var block_type = tool.get_voxel(place_pos)
+		
+		if block_type == TerrainData.AIR:
+			tool.set_voxel(place_pos, place_block_type)
+
+
+
+
+## <--- OUTLINE_BOX --> ##
 
 func create_outline_cube() -> MeshInstance3D:
 	var cube = MeshInstance3D.new()
@@ -48,31 +85,6 @@ func create_outline_cube() -> MeshInstance3D:
 	
 	return cube
 
-func block_deystroy():
-	if Input.is_action_just_pressed("attack"):
-		var current_voxel_type = voxel_tool.get_voxel(current_targeted_block)
-		if current_voxel_type != TerrainData.AIR:
-			voxel_tool.set_voxel(current_targeted_block, TerrainData.AIR)
-
-func block_placement():
-	if Input.is_action_just_pressed("interact") && player_inventory.selected_item != null:
-		var item: Item = player_inventory.selected_item
-		
-		if !item.can_be_placed: return
-		
-		var place_block_type = item.block_model
-		var tool = voxel_terrain.get_voxel_tool()
-
-		# Optional: set channel and mode
-		tool.set_channel(VoxelBuffer.CHANNEL_TYPE)
-
-		var place_pos = current_targeted_block + Vector3i(current_target_block_normal)
-
-		var block_type = tool.get_voxel(place_pos)
-		
-		if block_type == TerrainData.AIR:
-			tool.set_voxel(place_pos, place_block_type)
-		
 func update_outline() -> void:
 	if interaction_ray.is_colliding():
 		var hit_position = interaction_ray.get_collision_point()
@@ -84,5 +96,39 @@ func update_outline() -> void:
 		outline_cube.visible = true
 	else:
 		outline_cube.visible = false
+
+
+
+## <--- COLLISION CHECKING BODY ---> ##
+
+func create_outline_block_collision() -> Area3D:
+	var area: Area3D = Area3D.new()
+	var collision_body: CollisionShape3D = CollisionShape3D.new()
+	
+	var collision_shape: Shape3D = BoxShape3D.new()
+	collision_shape.size = Vector3(1, 1, 1)
+	
+	collision_body.shape = collision_shape
+	
+	area.collision_layer = 0
+	area.collision_mask = 2
+	
+	add_child(area)
+	area.add_child(collision_body) 
+	
+	return area
+
+func update_collision_check_body()-> void:
+	var place_pos = Vector3(current_targeted_block) + current_target_block_normal + Vector3(0.5, 0.5, 0.5)
+	collision_check_body.global_position = place_pos
+	
+func can_place_block() -> bool:
+	var bodies = collision_check_body.get_overlapping_bodies()
+	
+	for body in bodies:
+		if body.is_in_group("player"):
+			return false
+	
+	return true
 		
 		
